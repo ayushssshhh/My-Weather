0cb8632885d1f93d589acf5271217137464cbae1
ayushssshhh <ayushssshhh@gmail.com>
2023-03-30 13:22:43 +0530

    v1.0.1 container working

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+// module.exports = getLocation;
+
+// $("body").ready(function () {
+//     navigator.geolocation.getCurrentPosition((success) => {
+//         let { latitude, longitude } = success.coords;
+//         getLocation(latitude , longitude)
+//         console.log(success);
+//     })
+// })
+
+// function getLocation(latitude , longitude){
+//     let location = {
+//         lat : latitude,
+//         lon : longitude,
+//     }
+//     console.log(location);
+// }
+
+
+
